106f8532af738b5ee45231cbbe9a79f1076ae447
bakhitaismail <bakhitaismailk@gmail.com>
2022-06-18 08:00:54 +0300

    mobileapp commit

--- app/src/main/java/dev/bakhita/workoutlog/HomeActivity.kt
+++ app/src/main/java/dev/bakhita/workoutlog/HomeActivity.kt
@@ -3,14 +3,44 @@ package dev.bakhita.workoutlog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var bottom_navigation:BottomNavigationView
+    lateinit var fcvHome:FragmentContainerView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        castViews()
+        setupBottomNav()
+
     }
-//    tvLogin.setOnClickListener {
-//        val intent= Intent(this,LogInActivity::class.java)
-//        startActivity(intent)
-//    }
+    fun castViews(){
+        bottom_navigation=findViewById(R.id.bottom_navigation)
+        fcvHome=findViewById(R.id.fcvHome)
+    }
+    fun setupBottomNav(){
+        bottom_navigation.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.plan ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, PlanFragment()).commit()
+                    true
+                }
+                R.id.track ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, TrackFragment()).commit()
+                    true
+                }
+                R.id.profile -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, ProfileFragment()).commit()
+                    true
+                }
+                else-> false
+            }
+        }
+
+    }
+
 }
